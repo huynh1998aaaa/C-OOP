863ec586951d6f475022688637c8fb546aa5d77c
huynh nguyen <36124630+huynh1998aaaa@users.noreply.github.com>
2019-04-22 00:08:34 +0700

    Update time2.cpp

--- time2.cpp
+++ time2.cpp
@@ -10,30 +10,35 @@ private:
 	int p;
 	int g;
 public:
+	// cau tu 0 tham so
 	TIME()
 	{
 		h=0;
 		p=0;
 		g=0;
 	}
+	// cau tu 1 tham so
 	TIME(int a)
 	{
 		h=a;
 		p=0;
 		g=0;
 	}
+	// cau tu 2 tham so
 	TIME(int a,int b)
 	{
 		h=a;
 		p=b;
 		g=0;
 	}
+	// cau tu 3 tham so
 	TIME(int a,int b,int c)
 	{
 		h=a;
 		p=b;
 		g=c;
 	}
+	// qua tai toan tu nhap
 	friend istream& operator >>(istream&in, TIME &a)
 	{
 		in>>a.h;
@@ -41,41 +46,45 @@ public:
 		in>>a.g;
 		return in;
 	}
+	// qua tai toan tu xuat
 	friend ostream& operator<<(ostream& out, TIME a)
 	{
 		out<<a.h<<":"<<a.p<<":"<<a.g;
 		return out;
 	}
+	// doi thoi gian ve giay
 	int  CountSecond()
 	{
 		int t;
 		t=h*3600+p*60+g;
 		return t;
 	}
+	// lam chuan thoi gian
 	void advance()
 	{
-    while(g>=60)
-    {
-      p=p+1;
-      g=g-60;
-    }
-    while(p>=60)
-    {
-      h=h+1;
-      p=p-60;
-    }
+    	while(g>=60)
+    	{
+     	 p=p+1;
+      	g=g-60;
+    	}
+    	while(p>=60)
+    	{
+     	 h=h+1;
+    	  p=p-60;
+    	}
 
-    while(h>24)
-    {  
-    	h=h-24;
-    }
-    if(h==24)
-    {
-    	h=0;
-    }
+    	while(h>24)
+    	{  
+    		h=h-24;
+    	}
+    	if(h==24)
+    	{
+    		h=0;
+    	}
 	}
+	// qua tai toan tu cong
 	TIME operator +(int b)
-	{
+	{// cong gio voi giay
 		TIME c;
 		c.h=h;
 		c.p=p;
@@ -84,7 +93,7 @@ public:
 		return c;
 	}
 	friend TIME operator +(int b,TIME a)
-	{
+	{// cong giay voi gio
 		TIME c;
 		c.h=a.h;
 		c.p=a.p;
@@ -93,7 +102,7 @@ public:
 		return c;
 	}
 	TIME operator +(TIME b)
-	{
+	{// cong gio voi gio
 		TIME c;
 		c.h=h+b.h;
 		c.p=p+b.p;
@@ -101,9 +110,9 @@ public:
 		c.advance();
 		return c;
 	}
-		//  tru gio
+		//  qua tai toan tu tru
 	TIME operator -(int b)
-	{
+	{// tru giay
 		TIME c;
 		c.h=h;
 		c.p=p;
@@ -113,7 +122,7 @@ public:
 	}
 	
 	friend TIME operator -(int b,TIME a)
-	{
+	{//giay tru gio
 		TIME c;
 		c.h=a.h;
 		c.p=a.p;
@@ -123,7 +132,7 @@ public:
 	}
 	
 	TIME operator -(TIME b)
-	{
+	{// gio tru gio
 		TIME c;
 		c.h=h-b.h;
 		c.p=p-b.p;
